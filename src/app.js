0d10ed79a48db071c832c4e599577f296550cd37
louisetbiz <louisetbiz@gmail.com>
2023-05-14 22:05:33 +0200

    JS dropdown data and structure

--- src/app.js
+++ src/app.js
@@ -1,20 +1,47 @@
-setInterval(function(){
+function updateTime(){
 //PARIS
 let parisElement = document.querySelector("#paris");
+if (parisElement){
 let parisDateElement = parisElement.querySelector(".date");
 let parisTimeElement = parisElement.querySelector(".time");
 let parisTime = moment().tz("Europe/Paris");
 parisDateElement.innerHTML = parisTime.format("dddd, Do, YYYY");
-parisTimeElement.innerHTML = parisTime.format("hh:mm:ss [<small>]A[</small>]");
-}, 1000);
+parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
+}
 
-
-setInterval(function(){
 //LISBON
 let lisbonElement = document.querySelector("#lisbon");
+if (lisbonElement){
 let lisbonDateElement = lisbonElement.querySelector(".date");
 let lisbonTimeElement = lisbonElement.querySelector(".time");
 let lisbonTime = moment().tz("Europe/Lisbon");
 lisbonDateElement.innerHTML = lisbonTime.format("dddd, Do, YYYY");
-lisbonTimeElement.innerHTML = lisbonTime.format("hh:mm:ss [<small>]A[</small>]");
-}, 1000);
+lisbonTimeElement.innerHTML = lisbonTime.format("h:mm:ss [<small>]A[</small>]");
+}}
+
+
+function updateCity(event){
+let cityTimeZone = event.target.value;
+let cityTime = moment().tz(cityTimeZone);
+let cityName = cityTimeZone.replace("_"," ").split("/")[1];
+let citiesElement = document.querySelector("#cities");
+citiesElement.innerHTML =  `
+
+            
+        <div class="city"> 
+        <div id="cityName"> ${cityName}
+        <div class="date"> ${cityTime.format("ddd, Do YYYY")}</div></div>
+        <div class="time">${cityTime.format("h:mm:ss [<small>]A[</small>]")}</div></div>
+
+
+`}
+
+
+updateTime();
+setInterval(updateTime, 1000);
+
+
+let citiesSelectElement = document.querySelector("#city");
+
+
+citiesSelectElement.addEventListener("change", updateCity);
